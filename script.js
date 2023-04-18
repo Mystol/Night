a2430270b6f81943eedcafb1a6e3db41800c7d5b
Mystol <marcnouwligbeto@gmail.com>
2023-04-18 17:16:10 +0100

    add files style and script

---
+++ script.js
@@ -0,0 +1,46 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+let btn = document.querySelector(".nav-user-icon");
+
+let body = document.querySelector(".container")
+
+
+btn.onclick = function(){
+    console.log('ok')
+    settingsmenu.classList.toggle("settings-menu-height");
+
+    body.onclick = function(){
+        if(settingsmenu.classList.contains("settings-menu-height")){
+            settingsmenu.classList.remove("settings-menu-height")
+        }
+        
+    }
+
+}
+
+
+
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+if(localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+} 
+else if(localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+} 
+else {
+    localStorage.setItem("theme", "light");
+}
+localStorage.setItem("theme", "dark");
+localStorage.getItem("theme");
